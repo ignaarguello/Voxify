5e104c284284fdfc476cf5dcaecdb4ec92eb28d7
ignaarguello <igna.arguello2110@hotmail.com>
2025-07-01 12:11:59 -0300

    Add/Footer y componentes de fila - 2

--- Cliente/src/componentes/MainContent/MainContent.jsx
+++ Cliente/src/componentes/MainContent/MainContent.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 
 //Estilos
 import './MainContent.css'
+
+//Componentes
 import VideoPlayer from '../VideoPlayer/VideoPlayer'
 import HeaderContent from '../HeaderContent/HeaderContent'
+import Footer from '../Footer/Footer'
 
 export default function MainContent() {
   return (
     <div id='MainContent'>
-        <HeaderContent titulo='Dashboard' />
+      <HeaderContent titulo='Dashboard' />
       <section id='SeccionFila_1'>
         <VideoPlayer />
         <div id='ContenedorImage'>
           <h2 id='TituloPruebaGratis'>Prueba Gratis</h2>
         </div>
       </section>
-      <section  id='SeccionFila_2'>
-          
+      <section id='SeccionFila_2'>
+        <Footer />
       </section>
     </div>
   )
