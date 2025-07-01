8fd2406d35540add9b54fdf3fa0f928cf261e4b3
ignaarguello <igna.arguello2110@hotmail.com>
2025-07-01 00:38:04 -0300

    Add/Nuevas mejoras y cambios

--- Cliente/src/componentes/MainContent/MainContent.jsx
+++ Cliente/src/componentes/MainContent/MainContent.jsx
@@ -8,12 +8,15 @@ import HeaderContent from '../HeaderContent/HeaderContent'
 export default function MainContent() {
   return (
     <div id='MainContent'>
-      <section id='SeccionFila_1'>
         <HeaderContent titulo='Dashboard' />
+      <section id='SeccionFila_1'>
         <VideoPlayer />
+        <div id='ContenedorImage'>
+          <h2 id='TituloPruebaGratis'>Prueba Gratis</h2>
+        </div>
       </section>
-      <section id='SeccionFila_2'>
-          Seccion 2
+      <section  id='SeccionFila_2'>
+          
       </section>
     </div>
   )
