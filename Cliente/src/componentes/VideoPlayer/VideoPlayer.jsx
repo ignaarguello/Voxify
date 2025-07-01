be41c3df3bcc7c0785ff95fc90de1868271055b1
ignaarguello <igna.arguello2110@hotmail.com>
2025-06-30 22:35:05 -0300

    Add/Primeros armados y correciones de responsive

--- Cliente/src/componentes/VideoPlayer/VideoPlayer.jsx
+++ Cliente/src/componentes/VideoPlayer/VideoPlayer.jsx
@@ -3,19 +3,23 @@ import React from 'react'
 //ReactPlayer
 import ReactPlayer from 'react-player'
 
+//Estilos
+import './VideoPlayer.css'
+
 export default function VideoPlayer() {
     const UrlVideo = 'https://videos.pond5.com/young-hispanic-man-singing-song-footage-173778394_main_xxl.mp4'
 
     return (
-
+        <div id='ContenedorVideoPlayer'>
             <ReactPlayer
                 src={UrlVideo}
                 playing={true}
                 muted={true}
                 loop={true}
-                width={'50rem'}
+                width={'100%'}
                 height={'auto'}
-                style={{borderRadius: '30px', margin:'1.5rem'}}
+                style={{borderRadius: '30px'}}
             />
+        </div>
     )
 }
