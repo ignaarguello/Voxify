1bb6289a6e6db83a0a40d14d29186619d98aada4
ignaarguello <igna.arguello2110@hotmail.com>
2025-07-09 11:43:52 -0300

    Add/Footer

--- Cliente/src/componentes/Footer/Footer.jsx
+++ Cliente/src/componentes/Footer/Footer.jsx
@@ -6,7 +6,10 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div id='Footer'>
-      
+      {/* Menu Crear */}
+      <div id='MenuCrearNuevaSesion'>
+        <span>Nueva Sesión</span>
+      </div>
     </div>
   )
 }
