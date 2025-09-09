b920493c03f8d997dfd7e1ad1ac2d87edf55d179
ignaarguello_ <arguello.ignacio_dev@hotmail.com>
2025-09-09 12:05:29 -0300

    Add/Mejoras y Footer

--- Cliente/src/componentes/Footer/Footer.jsx
+++ Cliente/src/componentes/Footer/Footer.jsx
@@ -6,11 +6,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div id='Footer'>
-      {/* Menu Crear */}
-      <div id='ToogleMenuFooter'>
-        <span className='ToggleBotonFooter BotonCrearSesion' onClick={() => alert('Iniciando Sesion...')}>Iniciar Sesion</span>
-        <span  className='ToggleBotonFooter  BotonFinalizarSesion'onClick={() => alert('Cerrando Sesion...')}>Finalizar Sesion</span>
-      </div>
+      <h2 id='TituloVoxifyWebApp'>Iniciar Sesion</h2>
     </div>
   )
 }
